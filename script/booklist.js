441edc0ff9e38999b89ddee5b982aa7746b8403c
irenegiree <irenegiree@gmail.com>
2023-04-08 01:56:07 -0700

    set up booklist, books page and some other css fixes

---
+++ script/booklist.js
@@ -0,0 +1,113 @@
+var booklist= {
+    "Charles Dickens": [
+        {
+            "name":"Oliver Twist",
+            "price":20.5
+        }, 
+        {
+            "name":"Great Expectations",
+            "price":18.5
+        }, 
+        {
+            "name":"A Tale of Two Cities",
+            "price":19.5
+        },
+        {
+            "name":"Hard Times",
+            "price":21.5
+        },
+        {
+            "name":"Bleak House",
+            "price":22
+        }
+    ],
+    "Stephen King": [
+        {
+            "name":"The Shining",
+            "price":20.5
+        }, 
+        {
+            "name":"Billy Summers",
+            "price":18.5
+        }, 
+        {
+            "name":"Insomnia",
+            "price":19.5
+        },
+        {
+            "name":"Dreamcatcher",
+            "price":21.5
+        },
+        {
+            "name":"The Long Walk",
+            "price":22
+        }
+    ],
+    "Charles Dickens": [
+        {
+            "name":"The Handmaid's Tale",
+            "price":20.5
+        }, 
+        {
+            "name":"Lady Oracle",
+            "price":18.5
+        }, 
+        {
+            "name":"You Are Happy",
+            "price":19.5
+        },
+        {
+            "name":"Good Bones",
+            "price":21.5
+        },
+        {
+            "name":"The Heart Goes Last",
+            "price":22
+        }
+    ],
+    "Charles Dickens": [
+        {
+            "name":"And Still I Rise",
+            "price":20.5
+        }, 
+        {
+            "name":"Woman Work",
+            "price":18.5
+        }, 
+        {
+            "name":"Life Doesn’t Frighten Me",
+            "price":19.5
+        },
+        {
+            "name":"A Song Flung Up To Heaven",
+            "price":21.5
+        },
+        {
+            "name":"Angelina of Italy",
+            "price":22
+        }
+    ],
+    "Charles Dickens": [
+        {
+            "name":"The Notebook",
+            "price":20.5
+        }, 
+        {
+            "name":"Message in a Bottle",
+            "price":18.5
+        }, 
+        {
+            "name":"The Guardian",
+            "price":19.5
+        },
+        {
+            "name":"A Walk to Remember",
+            "price":21.5
+        },
+        {
+            "name":"Every Breath",
+            "price":22
+        }
+    ],
+
+}
