a5bb1995092843b5891df793d15be7cf8f82f3c8
irenegiree <irenegiree@gmail.com>
2023-04-08 03:02:56 -0700

    Created book cards and auto populated them with javascript.

--- script/booklist.js
+++ script/booklist.js
@@ -43,7 +43,7 @@ var booklist= {
             "price":22
         }
     ],
-    "Charles Dickens": [
+    "Margaret Atwood": [
         {
             "name":"The Handmaid's Tale",
             "price":20.5
@@ -65,7 +65,7 @@ var booklist= {
             "price":22
         }
     ],
-    "Charles Dickens": [
+    "Maya Angelou": [
         {
             "name":"And Still I Rise",
             "price":20.5
@@ -87,7 +87,7 @@ var booklist= {
             "price":22
         }
     ],
-    "Charles Dickens": [
+    "Nicholas Sparks": [
         {
             "name":"The Notebook",
             "price":20.5
@@ -110,4 +110,50 @@ var booklist= {
         }
     ],
 
+}
+
+for (var x in booklist) {
+    for(var y in booklist[x] ) {
+    var b = booklist[x][y]
+    console.log(b);
+    var newDiv = document.createElement('div');
+    newDiv.className = "book_card";
+    var newImg = document.createElement('img');
+    newImg.src="../images/book1.jpg";
+    newImg.className="book-img";
+    newDiv.appendChild(newImg);
+    var newP1 = document.createElement('p');
+    newP1.className="book-info";
+    newP1.innerHTML=`Name: ${b.name}`;
+    newDiv.appendChild(newP1);
+    var newP2 = document.createElement('p');
+    newP2.className="book-info";
+    newP2.innerHTML=`Author: ${x}`;
+    newDiv.appendChild(newP2);
+    var newP3 = document.createElement('p');
+    newP3.className="book-info";
+    newP3.innerHTML=`Price: ${b.price}`;
+    newDiv.appendChild(newP3);
+    var btnDiv = document.createElement('div');
+    btnDiv.id="numeric-btns";
+    var btn1 = document.createElement('button');
+    btn1.innerHTML="-";
+    btnDiv.appendChild(btn1);
+    var numberInput = document.createElement('input');
+    numberInput.type="number";
+    numberInput.min="0";
+    numberInput.max="20";
+    numberInput.value="0";
+    btnDiv.appendChild(numberInput);
+    var btn2 = document.createElement('button');
+    btn2.innerHTML="+";
+    btnDiv.appendChild(btn2);
+    newDiv.appendChild(btnDiv);
+    var btn3 = document.createElement('button');
+    btn3.innerHTML="Add to Cart";
+    newDiv.appendChild(btn3);
+    var targetDiv = document.getElementById("booklist");
+    targetDiv.appendChild(newDiv);
+    console.log("target: "+targetDiv);
+    } 
 }
